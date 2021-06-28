e2d9650bcbc75bc38fd44198652463fb6adecdfe
Gijs Buis <gijs.buis@me.com>
2021-06-28 12:39:08 +0200

    added Sanctum authentication functionality with register, login and logout routes

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RecipeController;
 use App\Http\Controllers\InstructionController;
 use App\Http\Controllers\IngredientController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,16 +19,24 @@ use App\Http\Controllers\IngredientController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
+// Public routes
 Route::get('/recipes', [RecipeController::class, 'index']);
 Route::get('/recipes/{recipe}', [RecipeController::class, 'show']);
-Route::post('/recipes', [RecipeController::class, 'store']);
 
 Route::get('/recipes/{recipe}/instructions', [RecipeController::class, 'show_instructions']);
-Route::post('/recipes/{recipe}/instructions', [InstructionController::class, 'store']);
 
 Route::get('/recipes/{recipe}/ingredients', [RecipeController::class, 'show_ingredients']);
-Route::post('/recipes/{recipe}/ingredients', [IngredientController::class, 'store']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/recipes', [RecipeController::class, 'store']);
+
+    Route::post('/recipes/{recipe}/instructions', [InstructionController::class, 'store']);
+    
+    Route::post('/recipes/{recipe}/ingredients', [IngredientController::class, 'store']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
