bc93b0026fad58bc790d60536b0410c87d8f88de
Gijs Buis <gijs.buis@me.com>
2021-07-04 11:50:16 +0200

    changed response type of search function to return json object

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -17,7 +17,7 @@ class SearchController extends Controller
         $recipes = Recipe::where('description', 'LIKE', '%'.$request->search.'%')->get();
 
         if (count($recipes) > 0 ) {
-            return response($recipes);
+            return response()->json(['message'=>'Success', 'data'=>$recipes], 200);
         } else {
             return response('Geen recepten gevonden');
         }
